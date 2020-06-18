c598469a7f2d63405260170ae4f2ee4876a87eff
Aleksandr <markovkin88@gmail.com>
2020-06-18 21:36:14 +0300

    7: Installed cors

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors')
+
+const saltRounds = 10;
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
   users: [
@@ -22,6 +27,13 @@ const database = {
       entries: 0,
       joined: new Date()
     }
+  ],
+  logi: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com'
+    }
   ]
 }
 
@@ -30,6 +42,16 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+  bcrypt.compare("apples",
+    "$2b$10$pXtoZ2g.MbkzpInNvJdgY.bP0WWwxZEcJIC0BFvbVEnhzB4gkHayW",
+    function (err, result) {
+      console.log('first guess', result);
+    });
+  bcrypt.compare("veggies",
+    "$2b$10$pXtoZ2g.MbkzpInNvJdgY.bP0WWwxZEcJIC0BFvbVEnhzB4gkHayW",
+    function (err, result) {
+      console.log('second guess', result);
+    });
   if (req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password) {
     res.json('success');
@@ -39,7 +61,10 @@ app.post('/signin', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  const {email, name, password} = req.body;
+  const { email, name, password } = req.body;
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    console.log(hash);
+  });
   database.users.push({
     id: '125',
     name: name,
@@ -48,34 +73,34 @@ app.post('/register', (req, res) => {
     entries: 0,
     joined: new Date()
   })
-  res.json(database.users[database.users.length-1]);
+  res.json(database.users[database.users.length - 1]);
 })
 
 app.get('/profile/:id', (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   let found = false;
   database.users.forEach(user => {
-    if(user.id === id) {
+    if (user.id === id) {
       found = true;
       return res.json(user);
     }
   })
-  if(!found) {
+  if (!found) {
     res.status(400).json('not found');
   }
 })
 
 app.post('/image', (req, res) => {
-  const {id} = req.body;
+  const { id } = req.body;
   let found = false;
   database.users.forEach(user => {
-    if(user.id === id) {
+    if (user.id === id) {
       found = true;
       user.entries++;
       return res.json(user.entries);
     }
   })
-  if(!found) {
+  if (!found) {
     res.status(400).json('not found');
   }
 })

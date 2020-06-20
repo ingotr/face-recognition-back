cc1bfd322b9f2912dd92e3a0f8046f9742a653c9
Aleksandr <markovkin88@gmail.com>
2020-06-20 21:53:07 +0300

    12: Updated register route with transaction to update db with new user's data

--- server.js
+++ server.js
@@ -77,18 +77,28 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  bcrypt.hash(password, saltRounds, function (err, hash) {
-    console.log(hash);
-  });
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date()
-    }).then(user => {
-      res.json(user[0]);
+  const hash = bcrypt.hashSync(password, saltRounds);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
     })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          }).then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+  })
     .catch(err => res.status(400).json('unable to register'))
 })
 
@@ -108,12 +118,12 @@ app.get('/profile/:id', (req, res) => {
 app.put('/image', (req, res) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 })
 
 app.listen(3000, () => {

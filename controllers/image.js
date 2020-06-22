c6e2c6da5269d843374f2215766310afe26a8a39
Aleksandr <markovkin88@gmail.com>
2020-06-22 22:06:42 +0300

    16: Installed Clarifai api. Moved api key from frontend to backend. Added new route imageurl for api work

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '1af38e65d20b44dbb4b6abf0cf4eed1c'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(
+      'c0c0ac362b03416da06ab3fa36fb58e3',req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage: handleImage
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
 }
